d940f88818221ff1b63aefe234c4fa1de43abc12
baba_S <shotababa1206@gmail.com>
2020-06-22 20:15:22 +0900

    first commit

---
+++ Scripts/IDisposableExt.cs
@@ -0,0 +1,26 @@
+﻿using Kogane.Internal;
+using System;
+using UnityEngine;
+
+namespace Kogane
+{
+	public static class IDisposableExt
+	{
+		public static void SetLink( this IDisposable self, GameObject gameObject )
+		{
+			var listener = gameObject.GetComponent<OnDestroyListener>();
+
+			if ( listener == null )
+			{
+				listener = gameObject.AddComponent<OnDestroyListener>();
+			}
+
+			listener.OnDestroyed += () => self.Dispose();
+		}
+
+		public static void SetLink( this IDisposable self, Component component )
+		{
+			SetLink( self, component.gameObject );
+		}
+	}
+}
---
+++ Scripts/OnDestroyListener.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	[DisallowMultipleComponent]
+	[AddComponentMenu( "" )]
+	internal sealed class OnDestroyListener : MonoBehaviour
+	{
+		public event Action OnDestroyed;
+
+		private void OnDestroy()
+		{
+			OnDestroyed?.Invoke();
+			OnDestroyed = null;
+		}
+	}
+}
